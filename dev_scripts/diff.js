0d09211579a75c7581207e0786b4f266367c0aa3
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-12-26 13:59:52 +1300

    developer scripts

---
+++ dev_scripts/diff.js
@@ -0,0 +1,65 @@
+const measurements = 10;
+
+const server1 = 'http://45.13.225.104:8080';
+const server2 = 'http://45.148.60.5:8080';
+
+const code = `
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, change_clear_color)
+        .run();
+    }
+
+    fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn change_clear_color(input: Res<Input<KeyCode>>, mut clear_color: ResMut<ClearColor>, mut state: Local<bool>) {
+    if input.just_pressed(KeyCode::Space) {
+    *state = !*state;
+    if *state {
+        clear_color.0 = Color::PURPLE;
+    } else {
+        clear_color.0 = Color::RED;
+    }
+    }
+}
+`;
+
+async function measureResponseTime(url) {
+    const startTime = new Date();
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: code,
+        });
+
+        const _body = await response.blob();
+
+        const endTime = new Date();
+        const responseTime = endTime - startTime;
+        return responseTime;
+    } catch (error) {
+        console.error(`Error for ${url}: ${error.message}`);
+        return Infinity;
+    }
+}
+
+async function main() {
+    for (let n = 0; n < measurements; n++) {
+        const results = await Promise.all([
+            measureResponseTime(server1),
+            measureResponseTime(server2),
+        ]);
+
+        console.log(`Fat: ${results[0]}ms`);
+        console.log(`Small: ${results[1]}ms`);
+    }
+}
+
+main();
---
+++ dev_scripts/test.js
@@ -0,0 +1,82 @@
+const code = `
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Circle
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+
+    // Rectangle
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 100.0)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        ..default()
+    });
+
+    // Quad
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes
+            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+            .into(),
+        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
+        ..default()
+    });
+
+    // Hexagon
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
+        transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
+        ..default()
+    });
+}
+`;
+
+async function app() {
+    while (true) {
+        console.log("Starting new request");
+        const start = Date.now();
+        const res = await fetch("http://45.13.225.104:8080", {
+            method: "POST",
+            headers: {
+                "content-type": "text/rust"
+            },
+            body: code,
+        });
+
+        const end = Date.now();
+        if (!res.ok) {
+            console.log(`Request failed: ${res.status} - ${res.statusText}\n${await res.text()}`);
+        } else {
+            console.log("Request success");
+        }
+        console.log(`Execution time: ${end - start} ms`);
+        console.log();
+
+        await new Promise(r => setTimeout(r, 1000));
+    }
+}
+
+app();
