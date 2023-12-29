64b9ece6808df46e0a50edecdb2f3e40c296fd11
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-12-29 14:19:04 +1300

    move compile function to its own file, use CF-Connecting-IP header, add Cool-Auth

---
+++ bevy_compile_api/src/compile.rs
@@ -0,0 +1,139 @@
+use crate::IMAGE;
+use log::{debug, error, info};
+use rouille::{Request, Response};
+use scopeguard::defer;
+use serde::Serialize;
+use std::{env, fs, process::Command};
+
+pub fn compile(id: usize, request: &Request) -> Response {
+    let docker_container_id = format!("compile.{id}");
+    let e500 = Response::text("Internal Server Error")
+        .with_status_code(500)
+        .with_additional_header("reference-number", id.to_string());
+
+    let Ok(body) = rouille::input::plain_text_body(request) else {
+        info!("{id}: Rejected for invalid body");
+        return Response::json(&OtherUserError {
+            msg: "Body must be plain utf8 text",
+        })
+        .with_status_code(400);
+    };
+
+    let dir = env::temp_dir()
+        .join("bevy_compile_api")
+        .join(id.to_string());
+    if let Err(err) = fs::create_dir_all(&dir) {
+        error!("{id}: Failed to create tempdir: {err:?}");
+        return e500;
+    }
+
+    defer! {
+        // This is cleanup so we don't return 500 on an error
+        if let Err(err) = fs::remove_dir_all(&dir) {
+            error!("{id}: Failed to remove tempdir: {err:?}");
+        }
+    }
+
+    if let Err(err) = fs::write(dir.join("main.rs"), body) {
+        error!("{id}: Failed to write main.rs to tempdir: {err:?}");
+        return e500;
+    }
+
+    let command_output = Command::new("docker")
+        .args([
+            "run",
+            "--name",
+            &docker_container_id,
+            "-v",
+            &format!("{}:/compile/src/", dir.display()),
+            IMAGE,
+            "sh",
+            "build.sh",
+        ])
+        .output();
+
+    defer! {
+        // This is cleanup so we don't return 500 on an error
+        if let Err(err) = Command::new("docker")
+            .args(["container", "rm", &docker_container_id])
+            .output()
+        {
+            error!("{id}: Failed to remove container: {err:?}");
+        }
+    }
+
+    let output = match command_output {
+        Ok(output) => output,
+        Err(err) => {
+            error!("{id}: Failed to execute docker process: {err:?}");
+            return e500;
+        }
+    };
+
+    // 101 is the rust compilers status code for failed to build (user error)
+    if output.status.code() == Some(101) {
+        info!("{id}: Build failed (user error)");
+        return Response::json(&BuildError {
+            msg: "Error building game",
+            stdout: String::from_utf8(output.stdout).unwrap_or("Contained invalid utf8".to_owned()),
+            stderr: String::from_utf8(output.stderr).unwrap_or("Contained invalid utf8".to_owned()),
+        })
+        .with_status_code(400);
+    }
+
+    if !output.status.success() {
+        error!(
+            "{id}: Build failed with code {} (server error)",
+            output.status
+        );
+        let stderr =
+            String::from_utf8(output.stderr).unwrap_or("Contained invalid utf8".to_string());
+        debug!("{id}: Stderr: {stderr}");
+        return e500;
+    }
+
+    let wasm = match fs::read(dir.join("game_bg.wasm")) {
+        Ok(wasm) => wasm,
+        Err(err) => {
+            error!("{id}: Failed to read game_bg.wasm: {err:?}");
+            return e500;
+        }
+    };
+
+    let mut js = match fs::read(dir.join("game.js")) {
+        Ok(js) => js,
+        Err(err) => {
+            error!("{id}: Failed to read game.js: {err:?}");
+            return e500;
+        }
+    };
+    // Remove two last lines of exports
+    js.resize(js.len() - 47, 0);
+    // Remove "import.meta.url" as it's not allowed outside a js module
+    js.drain(js.len() - 403 - 17..js.len() - 403);
+    // Add on the extra js
+    js.append(&mut include_bytes!("extra.js").to_vec());
+
+    let wasm_len = wasm.len();
+    let js_len = js.len();
+
+    let mut body = wasm;
+    body.append(&mut js);
+
+    Response::from_data("application/octet-stream", body)
+        .with_additional_header("reference-number", id.to_string())
+        .with_additional_header("wasm-content-length", wasm_len.to_string())
+        .with_additional_header("js-content-length", js_len.to_string())
+}
+
+#[derive(Serialize)]
+struct BuildError {
+    msg: &'static str,
+    stdout: String,
+    stderr: String,
+}
+
+#[derive(Serialize)]
+struct OtherUserError {
+    msg: &'static str,
+}
--- bevy_compile_api/src/main.rs
+++ bevy_compile_api/src/main.rs
@@ -1,16 +1,17 @@
 use chrono::{DateTime, Utc};
-use log::{debug, error, info};
+use log::{error, info};
 use rouille::{Request, Response, Server};
-use scopeguard::defer;
-use serde::Serialize;
 use std::{
-    env, fs,
-    process::Command,
+    fs,
+    net::Ipv4Addr,
     time::{Instant, SystemTime},
 };
 
+mod compile;
+
 const ADDRESS: &str = "0.0.0.0:443";
 const IMAGE: &str = "liamg737/comp";
+const AUTH_TOKEN: &str = include_str!("auth-token.txt");
 
 #[cfg(target_os = "linux")]
 const LOG_FOLDER_PATH: &str = "/var/log/bca";
@@ -52,165 +53,54 @@ fn main() {
 }
 
 fn request_handler(request: &Request) -> Response {
+    if request.header("Cool-Auth") != Some(AUTH_TOKEN) {
+        info!(
+            "Rejected request from {} because the auth header either did not exist or was incorrect",
+            request.remote_addr()
+        );
+        return Response::empty_404();
+    }
+
+    let Some(ip_str) = request.header("CF-Connecting-IP") else {
+        info!(
+            "Rejected request from {} because it did not contain a \"CF-Connecting-IP\" header",
+            request.remote_addr()
+        );
+        return Response::empty_404();
+    };
+
+    let Ok(ip) = ip_str.parse::<Ipv4Addr>() else {
+        return Response::empty_400();
+    };
+
     if request.raw_url() != "/compile" {
         info!(
             "Invalid path \"{}\" requested from {}",
             request.raw_url(),
             request.remote_addr()
         );
-        Response::empty_404()
-    } else if request.method() != "POST" {
+        return Response::empty_404();
+    }
+
+    if request.method() != "POST" {
         info!(
             "Invalid mathod \"{}\" requested from {}",
             request.method(),
             request.remote_addr()
         );
-        Response::text("Only the POST method is allowed")
+        return Response::text("Only the POST method is allowed")
             .with_status_code(405)
-            .with_additional_header("Allow", "POST")
-    } else {
-        let id = fastrand::usize(..);
-        info!("{id}: Serving new request from {}", request.remote_addr());
-        let start = Instant::now();
-
-        let response = compile(id, request)
-            .with_additional_header("access-control-allow-origin", "*")
-            .with_additional_header("access-control-expose-headers", "*");
-
-        info!("{id}: Finished in {:.2?}", start.elapsed());
-        response
-    }
-}
-
-fn compile(id: usize, request: &Request) -> Response {
-    let docker_container_id = format!("compile.{id}");
-    let e500 = Response::text("Internal Server Error")
-        .with_status_code(500)
-        .with_additional_header("reference-number", id.to_string());
-
-    let Ok(body) = rouille::input::plain_text_body(request) else {
-        info!("{id}: Rejected for invalid body");
-        return Response::json(&OtherUserError {
-            msg: "Body must be plain utf8 text",
-        })
-        .with_status_code(400);
-    };
-
-    let dir = env::temp_dir()
-        .join("bevy_compile_api")
-        .join(id.to_string());
-    if let Err(err) = fs::create_dir_all(&dir) {
-        error!("{id}: Failed to create tempdir: {err:?}");
-        return e500;
+            .with_additional_header("Allow", "POST");
     }
 
-    defer! {
-        // This is cleanup so we don't return 500 on an error
-        if let Err(err) = fs::remove_dir_all(&dir) {
-            error!("{id}: Failed to remove tempdir: {err:?}");
-        }
-    }
+    let id = fastrand::usize(..);
+    info!("{id}: Serving new request from {ip}");
+    let start = Instant::now();
 
-    if let Err(err) = fs::write(dir.join("main.rs"), body) {
-        error!("{id}: Failed to write main.rs to tempdir: {err:?}");
-        return e500;
-    }
+    let response = compile::compile(id, request)
+        .with_additional_header("access-control-allow-origin", "*")
+        .with_additional_header("access-control-expose-headers", "*");
 
-    let command_output = Command::new("docker")
-        .args([
-            "run",
-            "--name",
-            &docker_container_id,
-            "-v",
-            &format!("{}:/compile/src/", dir.display()),
-            IMAGE,
-            "sh",
-            "build.sh",
-        ])
-        .output();
-
-    defer! {
-        // This is cleanup so we don't return 500 on an error
-        if let Err(err) = Command::new("docker")
-            .args(["container", "rm", &docker_container_id])
-            .output()
-        {
-            error!("{id}: Failed to remove container: {err:?}");
-        }
-    }
-
-    let output = match command_output {
-        Ok(output) => output,
-        Err(err) => {
-            error!("{id}: Failed to execute docker process: {err:?}");
-            return e500;
-        }
-    };
-
-    // 101 is the rust compilers status code for failed to build (user error)
-    if output.status.code() == Some(101) {
-        info!("{id}: Build failed (user error)");
-        return Response::json(&BuildError {
-            msg: "Error building game",
-            stdout: String::from_utf8(output.stdout).unwrap_or("Contained invalid utf8".to_owned()),
-            stderr: String::from_utf8(output.stderr).unwrap_or("Contained invalid utf8".to_owned()),
-        })
-        .with_status_code(400);
-    }
-
-    if !output.status.success() {
-        error!(
-            "{id}: Build failed with code {} (server error)",
-            output.status
-        );
-        let stderr =
-            String::from_utf8(output.stderr).unwrap_or("Contained invalid utf8".to_string());
-        debug!("{id}: Stderr: {stderr}");
-        return e500;
-    }
-
-    let wasm = match fs::read(dir.join("game_bg.wasm")) {
-        Ok(wasm) => wasm,
-        Err(err) => {
-            error!("{id}: Failed to read game_bg.wasm: {err:?}");
-            return e500;
-        }
-    };
-
-    let mut js = match fs::read(dir.join("game.js")) {
-        Ok(js) => js,
-        Err(err) => {
-            error!("{id}: Failed to read game.js: {err:?}");
-            return e500;
-        }
-    };
-    // Remove two last lines of exports
-    js.resize(js.len() - 47, 0);
-    // Remove "import.meta.url" as it's not allowed outside a js module
-    js.drain(js.len() - 403 - 17..js.len() - 403);
-    // Add on the extra js
-    js.append(&mut include_bytes!("extra.js").to_vec());
-
-    let wasm_len = wasm.len();
-    let js_len = js.len();
-
-    let mut body = wasm;
-    body.append(&mut js);
-
-    Response::from_data("application/octet-stream", body)
-        .with_additional_header("reference-number", id.to_string())
-        .with_additional_header("wasm-content-length", wasm_len.to_string())
-        .with_additional_header("js-content-length", js_len.to_string())
-}
-
-#[derive(Serialize)]
-struct BuildError {
-    msg: &'static str,
-    stdout: String,
-    stderr: String,
-}
-
-#[derive(Serialize)]
-struct OtherUserError {
-    msg: &'static str,
+    info!("{id}: Finished in {:.2?}", start.elapsed());
+    response
 }
