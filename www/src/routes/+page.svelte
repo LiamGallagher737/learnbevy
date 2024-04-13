dec60177d65b908cfaa45d6fb7a23e17d1960fa6
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-04-13 16:54:26 +1200

    feat: super exciting landing page

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Hello, world!</h1>
+<main class="flex justify-center items-center flex-col h-screen">
+    <h1 class="text-xl">Coming soon...</h1>
+    <p>
+        In the meantime you can
+        <a class="underline" href="/playground">go to the playground</a>
+    </p>
+</main>
