582c8527a1533b05ff46d9ff9a06b95d2e570d83
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-04-13 13:15:45 +1200

    feat: code formatting

---
+++ www/src/lib/format.ts
@@ -0,0 +1,52 @@
+import { toast } from "svelte-sonner";
+import { consoleItems } from '$lib/components/console';
+import { get } from "svelte/store";
+import { editorCode } from "./components/editor";
+
+export async function formatCode() {
+    const promise = new Promise(async (resolve, reject) => {
+        const res = await fetch("https://rustfmt-api.fly.dev/format", {
+            method: "POST",
+            body: JSON.stringify({ code: get(editorCode) }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        const result = await res.json() as FmtResponse;
+        if (result.kind === "Success") {
+            editorCode.set(result.formatted_code);
+            resolve(result);
+        } else {
+            if (result.kind === "UserError")
+                consoleItems.update(items => [...items, { kind: 'Stdout', text: result.stderr }]);
+            reject(result);
+        }
+    });
+    toast.promise(promise, {
+        loading: 'Loading...',
+        success: 'Formatted successfully',
+        error: (e) => {
+            const err = e as FmtError;
+            if (err.kind === "UserError") return "Code could not be formatted";
+            return "Something went wrong on our end";
+        },
+    });
+}
+
+type FmtResponse = FmtSuccess | FmtUserError | FmtServerError;
+type FmtError = FmtUserError | FmtServerError;
+
+type FmtSuccess = {
+    kind: "Success";
+    formatted_code: string;
+}
+
+type FmtUserError = {
+    kind: "UserError";
+    stderr: string,
+}
+
+type FmtServerError = {
+    kind: "ServerError";
+}
--- www/src/routes/playground/Actions.svelte
+++ www/src/routes/playground/Actions.svelte
@@ -4,11 +4,12 @@
     import Share from 'lucide-svelte/icons/share';
     import BasicTooltip from '$lib/components/BasicTooltip.svelte';
     import { Button } from '$lib/components/ui/button';
+    import { formatCode } from '$lib/format';
 </script>
 
 <div class="flex flex-row gap-4">
     <BasicTooltip tooltip="Format">
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" on:click={() => formatCode()}>
             <Paintbrush class="h-4 w-4" />
         </Button>
     </BasicTooltip>
