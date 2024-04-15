8e2e250312ce6b9d4dc8bc32da17ce7f86ebad65
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-04-15 21:42:23 +1200

    working image (for single version, 0.13)

--- images/build.sh
+++ images/build.sh
@@ -2,4 +2,4 @@
 
 set -e
 cargo b --release --target wasm32-unknown-unknown --jobs 1
-wasm-bindgen --no-typescript --out-dir ./src/ --target web ./target/wasm32-unknown-unknown/release/game.wasm
+wasm-bindgen --no-typescript --out-dir /playground/src/ --target web /playground/target/wasm32-unknown-unknown/release/game.wasm
