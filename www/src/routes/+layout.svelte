e6af8b9190c9247affa32442279cbfefd8f375c9
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-05-21 21:34:41 +1200

    Add Mastodon Verification (#38)

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -4,6 +4,10 @@
     import { Toaster } from "$lib/components/ui/sonner";
 </script>
 
+<svelte:head>
+    <link rel="me" href="https://mastodon.social/@liam737" />
+</svelte:head>
+
 <ModeWatcher defaultMode="dark" />
 <Toaster />
 <slot />
