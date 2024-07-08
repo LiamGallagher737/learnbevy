6960adba8f3789bf9cdef351bb61d861cdc076a5
Liam Gallagher <liam@liamgallagher.dev>
2024-07-08 17:52:24 +1200

    open port in firewall

--- compile_server_os/configuration.nix
+++ compile_server_os/configuration.nix
@@ -19,6 +19,11 @@
     inputs.learnbevy.packages."${pkgs.system}".default
   ];
 
+  networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [ 53740 ];
+  };
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZJ0j9jx1q5diqgyK0r7j8Zi20ZuZIpp1QKLUmSHeRDycmPZIhdH6P0gkd8Am1Y/VK2K1XTLahApksIXBBox1KSt2GcLLfPBie3tOocxBNtoJdQrHu+2nRL9PsngOZi1wLvv8qoaidp9Rsi4p8SA58WJHsoVqPJNZNPDPvAEXBXy/Wc/JsYy8RSzkAaXbCXo1c2DgctbMjA3+GUl9UFLUf76yd6kUjlTR9Pk7rjYzbLel8yd7i4CjnvNu+tc+WrXzLabKBlS4EP+t9owCD0vb362hyD8Wxg6BrtJnwUABEbZeZlOQjYRmA+zG4Yics7d4C8eOmehufSym616MpEg8zW8qxTnnySbVpKNGv3JM45N7YcXM7mqdFoy/lkI4IgUn3AA6fQ9Jlk+c7vzC75FXarQ5Ln2q71fo/0XH/SOTdgsvmEL3SnAepTCpbfXLjvUs9x4WN2AofVcbybPvU4ytD4vo4r9z1vqaoVpHt/eJb2cbNGHQqEHeUsYRhGXjzWys= liam@desktop"
   ];
