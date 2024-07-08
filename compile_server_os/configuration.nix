14d19236f57d8e1c5bd6d1dc70c5f4d53d66d8f1
Liam Gallagher <liam@liamgallagher.dev>
2024-07-08 21:58:58 +1200

    tidy stuff up

--- compile_server_os/configuration.nix
+++ compile_server_os/configuration.nix
@@ -4,6 +4,7 @@
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
   ];
+
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
@@ -32,21 +33,22 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZJ0j9jx1q5diqgyK0r7j8Zi20ZuZIpp1QKLUmSHeRDycmPZIhdH6P0gkd8Am1Y/VK2K1XTLahApksIXBBox1KSt2GcLLfPBie3tOocxBNtoJdQrHu+2nRL9PsngOZi1wLvv8qoaidp9Rsi4p8SA58WJHsoVqPJNZNPDPvAEXBXy/Wc/JsYy8RSzkAaXbCXo1c2DgctbMjA3+GUl9UFLUf76yd6kUjlTR9Pk7rjYzbLel8yd7i4CjnvNu+tc+WrXzLabKBlS4EP+t9owCD0vb362hyD8Wxg6BrtJnwUABEbZeZlOQjYRmA+zG4Yics7d4C8eOmehufSym616MpEg8zW8qxTnnySbVpKNGv3JM45N7YcXM7mqdFoy/lkI4IgUn3AA6fQ9Jlk+c7vzC75FXarQ5Ln2q71fo/0XH/SOTdgsvmEL3SnAepTCpbfXLjvUs9x4WN2AofVcbybPvU4ytD4vo4r9z1vqaoVpHt/eJb2cbNGHQqEHeUsYRhGXjzWys= liam@desktop"
     ];
-    linger = true;
+    linger = true; # for podman
   };
 
   users.users.ferris = {
     isNormalUser = true;
     home = "/home/ferris";
     description = "The user that runs the server";
-    linger = true;
+    linger = true; # for podman
     packages = [
       pkgs.podman
-      pkgs.shadow
+      pkgs.shadow # podman needs this
       inputs.learnbevy.packages."${pkgs.system}".default
     ];
   };
 
+# Run the compile api as a service
   systemd.services.compile_api = {
     description = "Compile API Service";
     wantedBy = [ "multi-user.target" ];
@@ -58,6 +60,7 @@
     };
   };
 
+  # Pull the images every 15 minutes
   systemd.services.pull-learnbevy-images = {
     description = "Pull the learnbevy images on startup";
     serviceConfig = {
