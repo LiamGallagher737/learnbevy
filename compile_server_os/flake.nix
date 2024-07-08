daa8fe8982c20ac7d1a44a839b62bfe37b5495ce
Liam Gallagher <liam@liamgallagher.dev>
2024-07-08 14:45:30 +1200

    remove example configs

--- compile_server_os/flake.nix
+++ compile_server_os/flake.nix
@@ -13,34 +13,5 @@
           ./configuration.nix
         ];
       };
-      nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
-      # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
-      nixosConfigurations.digitalocean = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          { disko.devices.disk.disk1.device = "/dev/vda"; }
-          {
-            # do not use DHCP, as DigitalOcean provisions IPs using cloud-init
-            networking.useDHCP = nixpkgs.lib.mkForce false;
-
-            services.cloud-init = {
-              enable = true;
-              network.enable = true;
-
-              # not strictly needed, just for good measure 
-              datasource_list = [ "DigitalOcean" ];
-              datasource.DigitalOcean = { };
-            };
-          }
-          ./configuration.nix
-        ];
-      };
     };
 }
