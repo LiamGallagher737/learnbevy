df365182f82788e9d3ef478b3c3f0beea346d01c
Liam Gallagher <liam@liamgallagher.dev>
2024-07-11 13:03:16 +1200

    fetching readme and other data

--- tools/check_crate_updates/src/main.rs
+++ tools/check_crate_updates/src/main.rs
@@ -1,5 +1,8 @@
 use cargo_toml::Manifest;
-use std::{env, fs, io};
+use std::{env, fs, io, path};
+use ureq::{Agent, AgentBuilder};
+
+const EXCLUDE_CRATES: &[&str] = &["bevy", "rand", "rand_chacha", "wasm-bindgen"];
 
 fn main() -> io::Result<()> {
     let Some(manifest_dir) = env::args().nth(1) else {
@@ -9,6 +12,12 @@ fn main() -> io::Result<()> {
         ));
     };
 
+    let agent = AgentBuilder::new()
+        // Crates.io asks that you add a user agent with some way of contacting you
+        // https://crates.io/data-access#api
+        .user_agent("github.com/LiamGallagher737/learnbevy")
+        .build();
+
     println!("Searching {manifest_dir}");
 
     let _ = fs::read_dir(&manifest_dir)?
@@ -16,16 +25,76 @@ fn main() -> io::Result<()> {
         .map(|entry| entry.path())
         .filter(|path| path.to_string_lossy().ends_with(".Cargo.toml"))
         .inspect(|path| println!("Found manifest: {path:?}"))
-        .map(|path| (Manifest::from_path(&path), path))
-        .inspect(|(result, path)| {
-            if let Err(e) = result {
-                eprintln!("Failed to parse manifest: {path:?}\n{e}");
-            }
+        .filter_map(parse_manifest)
+        .map(|data| {
+            data.manifest
+                .dependencies
+                .iter()
+                .filter(|(name, _)| !EXCLUDE_CRATES.contains(&name.as_str()))
+                .map(|(name, _)| fetch_crate_data(name, agent.clone()))
+                .collect::<Vec<_>>()
         })
-        .filter_map(|(result, p)| result.map(|m| (m, p)).ok())
-        .for_each(|m| ());
+        .for_each(|_| ());
 
     println!("Complete");
 
     Ok(())
+}
+
+struct ManifestData {
+    path: path::PathBuf,
+    manifest: Manifest,
+}
+
+fn parse_manifest(path: path::PathBuf) -> Option<ManifestData> {
+    match Manifest::from_path(&path) {
+        Ok(manifest) => Some(ManifestData { path, manifest }),
+        Err(e) => {
+            eprintln!("Failed to parse {path:?}\n{e}");
+            None
+        }
+    }
+}
+
+struct CrateData {
+    versions: Vec<String>,
+    readme: String,
+}
+
+fn fetch_crate_data(name: &str, agent: Agent) -> Option<CrateData> {
+    let mut c = agent
+        .get(&format!("https://crates.io/api/v1/crates/{name}"))
+        .call()
+        .inspect_err(|e| eprintln!("Failed to fetch crate data for {name:?}\n{e}"))
+        .ok()?
+        .into_json::<CrateResponse>()
+        .inspect_err(|e| eprintln!("Failed to parse crate data for {name:?}\n{e}"))
+        .ok()?;
+
+    let readme_path = &c.versions[0].readme_path; // index 0 is latest
+
+    let readme = agent
+        .get(&format!("https://crates.io{readme_path}"))
+        .call()
+        .inspect_err(|e| eprintln!("Failed to fetch readme for {name:?}\n{e}"))
+        .ok()?
+        .into_string()
+        .inspect_err(|e| eprintln!("Failed to read readme for {name:?}\n{e}"))
+        .ok()?;
+
+    println!("Fetched all data for {name}");
+    let versions = c.versions.drain(..).map(|v| v.version).collect();
+    Some(CrateData { versions, readme })
+}
+
+#[derive(serde::Deserialize)]
+struct CrateResponse {
+    versions: Vec<CrateVersionResponse>,
+}
+
+#[derive(serde::Deserialize)]
+struct CrateVersionResponse {
+    #[serde(rename = "num")]
+    version: String,
+    readme_path: String,
 }
