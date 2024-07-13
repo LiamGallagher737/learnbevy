65d4132fed7652dbc0aa8fab7aba3fdc77294c0a
Liam Gallagher <liam@liamgallagher.dev>
2024-07-13 20:40:26 +1200

    format

--- www/src/routes/api/[bevyVersion]/crates/+server.ts
+++ www/src/routes/api/[bevyVersion]/crates/+server.ts
@@ -1,10 +1,12 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ params }) => {
-    return new Response(JSON.stringify({
-        crates: [
-            { name: 'bevy', versions: params.bevyVersion },
-            { name: 'rand', versions: '0.8.5' },
-        ]
-    }));
-}
+    return new Response(
+        JSON.stringify({
+            crates: [
+                { name: "bevy", versions: params.bevyVersion },
+                { name: "rand", versions: "0.8.5" },
+            ],
+        })
+    );
+};
