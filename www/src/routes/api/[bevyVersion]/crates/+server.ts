ea12eccb9ddca870310362456ab283d6fca970b2
Liam Gallagher <liam@liamgallagher.dev>
2024-07-13 21:28:21 +1200

    api impl

--- www/src/routes/api/[bevyVersion]/crates/+server.ts
+++ www/src/routes/api/[bevyVersion]/crates/+server.ts
@@ -1,12 +1,38 @@
-import type { RequestHandler } from "@sveltejs/kit";
+import { error, type RequestHandler } from "@sveltejs/kit";
+import TOML from 'smol-toml';
+
+const EXCLUDE_CRATES = ["wasm-bindgen"];
 
 export const GET: RequestHandler = async ({ params }) => {
+    const url = `https://raw.githubusercontent.com/LiamGallagher737/learnbevy/main/images/manifests/${params.bevyVersion}.Cargo.toml`;
+    const response = await fetch(url);
+
+    if (response.status === 404) {
+        error(404, {
+            message: 'Not found',
+        });
+    }
+
+    const text = await response.text();
+    const manifest = TOML.parse(text);
+
+    let crates = [];
+
+    for (const [name, value] of Object.entries(manifest.dependencies)) {
+        if (EXCLUDE_CRATES.includes(name)) continue;
+
+        if (typeof value === "string") {
+            crates.push({ name, version: value });
+        } else if (typeof value === "object" && typeof value.version === "string") {
+            crates.push({ name, version: value.version });
+        } else {
+            error(500, {
+                message: "Failed to parse crate " + name,
+            });
+        }
+    }
+
     return new Response(
-        JSON.stringify({
-            crates: [
-                { name: "bevy", version: params.bevyVersion },
-                { name: "rand", version: "0.8.5" },
-            ],
-        })
+        JSON.stringify({ crates })
     );
 };
