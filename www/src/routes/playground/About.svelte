52cf2574c4147a028ae524cba6b6db4b09eaab3e
Liam Gallagher <liam@liamgallagher.dev>
2024-07-18 16:35:08 +1200

    spelling and grammar

--- www/src/routes/playground/About.svelte
+++ www/src/routes/playground/About.svelte
@@ -11,9 +11,9 @@
     <section class="space-y-2">
         <h4 class="text-md text-base font-semibold">How does it work?</h4>
         <p class="text-muted-foreground">
-            Everytime you press the play button, the code in the editor along with your selected
+            Every time you press the play button, the code in the editor along with your selected
             Bevy version and Rust channel are sent to my server. Once there some extra code is added
-            and then it is compiled to wasm inside of a podman container. After it's compiled,
+            and then it is compiled to wasm inside of a podman container. After its compiled,
             wasm-bindgen works it's magic on the output which is then sent back to your browser
             along with the generated javascript from wasm-bindgen and the stdout of the compile
             process.
@@ -23,7 +23,7 @@
     <section class="space-y-2">
         <h4 class="text-md text-base font-semibold">How can I contribute?</h4>
         <p class="text-muted-foreground">
-            All the code is avaliable publicly on GitHub at
+            All the code is available publicly on GitHub at
             <a
                 href="https://github.com/LiamGallagher737/learnbevy"
                 class="text-primary underline"
@@ -33,7 +33,7 @@
             >.
         </p>
         <p class="text-muted-foreground">
-            Keep in mind that contributing doesn't mean having to write any code, it can be as
+            Remember that contributing doesn't mean having to write any code, it can be as
             simple as suggesting a new crate to be added!
         </p>
     </section>
