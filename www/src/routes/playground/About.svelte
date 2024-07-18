06784451d23155c0ac06b72083147906fabc2ba4
Liam Gallagher <liam@liamgallagher.dev>
2024-07-18 16:22:36 +1200

    contribution section

--- www/src/routes/playground/About.svelte
+++ www/src/routes/playground/About.svelte
@@ -5,7 +5,22 @@
 <Card.Header class="flex flex-row justify-between">
     <div class="grid gap-2">
         <Card.Title>About</Card.Title>
-        <Card.Description>About the Bevy playground</Card.Description>
     </div>
 </Card.Header>
-<Card.Content></Card.Content>
+<Card.Content class="text-pretty">
+    <Card.Title tag="h4" class="text-base">How can I contribute?</Card.Title>
+    <p class="pb-2 text-muted-foreground">
+        All the code is avaliable publicly on GitHub at
+        <a
+            href="https://github.com/LiamGallagher737/learnbevy"
+            class="text-primary underline"
+            target="_blank"
+        >
+            LiamGallagher737/learnbevy</a
+        >.
+    </p>
+    <p class="text-muted-foreground">
+        Keep in mind that contributing doesn't mean having to write any code, it can be as simple as
+        suggesting a new crate to be added!
+    </p>
+</Card.Content>
