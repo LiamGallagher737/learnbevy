9ff622ac6ceb50be2339b72388b108781d01b070
Liam Gallagher <liam@liamgallagher.dev>
2024-07-18 16:33:13 +1200

    how it works section

--- www/src/routes/playground/About.svelte
+++ www/src/routes/playground/About.svelte
@@ -7,20 +7,34 @@
         <Card.Title>About</Card.Title>
     </div>
 </Card.Header>
-<Card.Content class="text-pretty">
-    <Card.Title tag="h4" class="text-base">How can I contribute?</Card.Title>
-    <p class="pb-2 text-muted-foreground">
-        All the code is avaliable publicly on GitHub at
-        <a
-            href="https://github.com/LiamGallagher737/learnbevy"
-            class="text-primary underline"
-            target="_blank"
-        >
-            LiamGallagher737/learnbevy</a
-        >.
-    </p>
-    <p class="text-muted-foreground">
-        Keep in mind that contributing doesn't mean having to write any code, it can be as simple as
-        suggesting a new crate to be added!
-    </p>
+<Card.Content class="space-y-4 text-pretty">
+    <section class="space-y-2">
+        <h4 class="text-md text-base font-semibold">How does it work?</h4>
+        <p class="text-muted-foreground">
+            Everytime you press the play button, the code in the editor along with your selected
+            Bevy version and Rust channel are sent to my server. Once there some extra code is added
+            and then it is compiled to wasm inside of a podman container. After it's compiled,
+            wasm-bindgen works it's magic on the output which is then sent back to your browser
+            along with the generated javascript from wasm-bindgen and the stdout of the compile
+            process.
+        </p>
+    </section>
+
+    <section class="space-y-2">
+        <h4 class="text-md text-base font-semibold">How can I contribute?</h4>
+        <p class="text-muted-foreground">
+            All the code is avaliable publicly on GitHub at
+            <a
+                href="https://github.com/LiamGallagher737/learnbevy"
+                class="text-primary underline"
+                target="_blank"
+            >
+                LiamGallagher737/learnbevy</a
+            >.
+        </p>
+        <p class="text-muted-foreground">
+            Keep in mind that contributing doesn't mean having to write any code, it can be as
+            simple as suggesting a new crate to be added!
+        </p>
+    </section>
 </Card.Content>
