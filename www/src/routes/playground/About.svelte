ef446a5c46b41929b01fcd6efff0f2daa23cd2b8
Liam Gallagher <liam@liamgallagher.dev>
2024-07-18 16:03:03 +1200

    add about page (blank)

---
+++ www/src/routes/playground/About.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card";
+</script>
+
+<Card.Header class="flex flex-row justify-between">
+    <div class="grid gap-2">
+        <Card.Title>About</Card.Title>
+        <Card.Description>About the Bevy playground</Card.Description>
+    </div>
+</Card.Header>
+<Card.Content></Card.Content>
