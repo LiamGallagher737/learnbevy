f7dcbdb2608533a0e61e6e704d763cfbfc458739
Liam Gallagher <liam@liamgallagher.dev>
2024-10-07 15:14:18 +1300

    remove old versions

--- compile_api/src/config.rs
+++ compile_api/src/config.rs
@@ -5,10 +5,6 @@ pub fn image_for_config(version: Version, channel: Channel) -> String {
     let with_version = match version {
         Version::Main => "learnbevy-main",
         Version::V0_14 => "learnbevy-0.14",
-        Version::V0_13 => "learnbevy-0.13",
-        Version::V0_12 => "learnbevy-0.12",
-        Version::V0_11 => "learnbevy-0.11",
-        Version::V0_10 => "learnbevy-0.10",
     }
     .to_string();
     let image = match channel {
@@ -24,16 +20,13 @@ pub fn edit_code_for_version(code: &str, version: Version) -> String {
     match version {
         Version::Main => edit_code_v14(code),
         Version::V0_14 => edit_code_v14(code),
-        Version::V0_13 => edit_code_v11(code),
-        Version::V0_12 => edit_code_v11(code),
-        Version::V0_11 => edit_code_v11(code),
-        Version::V0_10 => edit_code_v10(code),
     }
 }
 
 /// This extra Rust code is added to every request.
 /// It currently is just an [AtomicBool](std::sync::atomic::AtomicBool) that defaults to false and
-/// a system which sends the Bevy exit event when the bool is set to true from the JavaScript.
+/// a system which sends the Bevy exit event when the bool is set to true from the
+/// JavaScript.config
 const EXTRA_RUST: &str = r#"
 static __EXIT_FLAG: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
 #[wasm_bindgen::prelude::wasm_bindgen]
@@ -42,7 +35,7 @@ pub fn __exit() {
 }
 fn __check_exit_flag(mut exit: bevy::ecs::event::EventWriter<bevy::app::AppExit>) {
     if __EXIT_FLAG.load(std::sync::atomic::Ordering::Relaxed) {
-        exit.send(bevy::app::AppExit);
+        exit.send(bevy::app::AppExit::Success);
     }
 }
 
@@ -81,26 +74,10 @@ mod __playground_dbg {
 
 /// Monifies the code in Bevy 0.14's style. Used by [edit_code_for_version].
 fn edit_code_v14(code: &str) -> String {
-    let code = edit_code_v11(code);
-    code.replace(
-        "exit.send(bevy::app::AppExit)",
-        "exit.send(bevy::app::AppExit::Success)",
-    )
-}
-
-/// Monifies the code in Bevy 0.11's style. Used by [edit_code_for_version].
-fn edit_code_v11(code: &str) -> String {
     let mut modified_code = code.replace(
         "App::new()",
         "App::new().add_systems(Update, __check_exit_flag)",
     );
-    modified_code.push_str(EXTRA_RUST);
-    modified_code
-}
-
-/// Monifies the code in Bevy 0.10's style. Used by [edit_code_for_version].
-fn edit_code_v10(code: &str) -> String {
-    let mut modified_code = code.replace("App::new()", "App::new().add_system(__check_exit_flag)");
     modified_code.push_str(EXTRA_RUST);
     modified_code
 }
@@ -125,14 +102,6 @@ impl std::fmt::Display for Channel {
 pub enum Version {
     #[serde(rename = "main")]
     Main,
-    #[serde(rename = "0.10")]
-    V0_10,
-    #[serde(rename = "0.11")]
-    V0_11,
-    #[serde(rename = "0.12")]
-    V0_12,
-    #[serde(rename = "0.13")]
-    V0_13,
     #[default]
     #[serde(rename = "0.14")]
     V0_14,
@@ -142,10 +111,6 @@ impl std::fmt::Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Version::Main => f.write_str("Main"),
-            Version::V0_10 => f.write_str("0.10"),
-            Version::V0_11 => f.write_str("0.11"),
-            Version::V0_12 => f.write_str("0.12"),
-            Version::V0_13 => f.write_str("0.13"),
             Version::V0_14 => f.write_str("0.14"),
         }
     }
