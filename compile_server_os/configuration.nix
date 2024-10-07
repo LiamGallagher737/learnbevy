b48550170584f4ea20e45e0e42a2c93aa9460d79
Liam Gallagher <liam@liamgallagher.dev>
2024-10-07 18:15:13 +1300

    new ssh key

--- compile_server_os/configuration.nix
+++ compile_server_os/configuration.nix
@@ -32,7 +32,7 @@
     hashedPassword = "!"; # nothing hashes to "!" so this effectively disables the password
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZJ0j9jx1q5diqgyK0r7j8Zi20ZuZIpp1QKLUmSHeRDycmPZIhdH6P0gkd8Am1Y/VK2K1XTLahApksIXBBox1KSt2GcLLfPBie3tOocxBNtoJdQrHu+2nRL9PsngOZi1wLvv8qoaidp9Rsi4p8SA58WJHsoVqPJNZNPDPvAEXBXy/Wc/JsYy8RSzkAaXbCXo1c2DgctbMjA3+GUl9UFLUf76yd6kUjlTR9Pk7rjYzbLel8yd7i4CjnvNu+tc+WrXzLabKBlS4EP+t9owCD0vb362hyD8Wxg6BrtJnwUABEbZeZlOQjYRmA+zG4Yics7d4C8eOmehufSym616MpEg8zW8qxTnnySbVpKNGv3JM45N7YcXM7mqdFoy/lkI4IgUn3AA6fQ9Jlk+c7vzC75FXarQ5Ln2q71fo/0XH/SOTdgsvmEL3SnAepTCpbfXLjvUs9x4WN2AofVcbybPvU4ytD4vo4r9z1vqaoVpHt/eJb2cbNGHQqEHeUsYRhGXjzWys= liam@desktop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICDRuZSMctfFCOR7goQF96iUOWa6sDys0Yoba8n7g6Dh" # github
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILn2Q2kGPvavzjMn9Bxe7v98llgYAKiXDCeSGLrG4vyq root@slc.compile.learnbevy.com" # github
     ];
     linger = true; # for podman
   };
