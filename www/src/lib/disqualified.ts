b12c341d1dfb825b9ed1edbac2c93383c06be810
Liam Gallagher <liam@liamgallagher.dev>
2024-10-12 17:08:34 +1300

    disqualified lib

---
+++ www/src/lib/disqualified.ts
@@ -0,0 +1,59 @@
+// Javascript port of https://github.com/bevyengine/disqualified
+
+export class TypePath {
+    private fullTypePath: string;
+
+    constructor(fullName: string) {
+        this.fullTypePath = fullName;
+    }
+
+    full() {
+        return this.fullTypePath;
+    }
+
+    short() {
+        let result = "";
+        let index = 0;
+        const endOfString = this.fullTypePath.length;
+
+        while (index < endOfString) {
+            const restOfString = this.fullTypePath.slice(index);
+
+            const specialCharacterIndex = restOfString.search(/[\s<>()[\],;]/);
+            if (specialCharacterIndex !== -1) {
+                const segmentToCollapse = restOfString.slice(0, specialCharacterIndex);
+                result += collapseTypeName(segmentToCollapse);
+
+                const specialCharacter = restOfString[specialCharacterIndex];
+                result += specialCharacter;
+
+                if (
+                    [">", ")", "]"].includes(specialCharacter) &&
+                    restOfString.startsWith("::", specialCharacterIndex + 1)
+                ) {
+                    result += "::";
+                    index += specialCharacterIndex + 3;
+                } else {
+                    index += specialCharacterIndex + 1;
+                }
+            } else {
+                result += collapseTypeName(restOfString);
+                index = endOfString;
+            }
+        }
+
+        return result;
+    }
+}
+
+function collapseTypeName(input: string) {
+    const segments = input.split("::");
+    const last = segments.pop();
+    const secondLast = segments.pop();
+
+    if (secondLast && /^[A-Z]/.test(secondLast)) {
+        return `${secondLast}::${last}`;
+    } else {
+        return last;
+    }
+}
