384f70aabc84fdcc3cbbf635d5221e5c5fe1d7a6
Liam Gallagher <liam@liamgallagher.dev>
2024-10-20 18:57:30 +1300

    dioxus

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }:
+let
+    pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+    pkgs-local = import inputs.nixpkgs-local { system = pkgs.stdenv.system; };
+in {
     languages.rust = {
         enable = true;
         channel = "stable";
+        targets = [
+            "wasm32-unknown-unknown"
+        ];
     };
     languages.javascript = {
         enable = true;
         npm.enable = true;
     };
-    packages = with pkgs; [
-        podman
-        cargo-watch
-        nodePackages.wrangler
-        lsof # for wrangler
+    packages = [
+        pkgs.podman
+        pkgs.cargo-watch
+        pkgs.tailwindcss
+        pkgs-local.dioxus-cli
+        pkgs-local.wasm-bindgen-cli
     ];
 }
---
+++ www/src/main.rs
@@ -0,0 +1,75 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_logger::tracing;
+
+#[derive(Clone, Routable, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(tracing::Level::INFO).expect("failed to init logger");
+    tracing::info!("starting app");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut count = use_signal(|| 0);
+    let mut text = use_signal(|| String::from("..."));
+
+    rsx! {
+        Link {
+            to: Route::Blog {
+                id: count()
+            },
+            "Go to blog"
+        }
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+            button {
+                onclick: move |_| async move {
+                    if let Ok(data) = get_server_data().await {
+                        tracing::info!("Client received: {}", data);
+                        text.set(data.clone());
+                        post_server_data(data).await.unwrap();
+                    }
+                },
+                "Get Server Data"
+            }
+            p { "Server data: {text}"}
+        }
+    }
+}
+
+#[server(PostServerData)]
+async fn post_server_data(data: String) -> Result<(), ServerFnError> {
+    tracing::info!("Server received: {}", data);
+    Ok(())
+}
+
+#[server(GetServerData)]
+async fn get_server_data() -> Result<String, ServerFnError> {
+    Ok("Hello from the server!".to_string())
+}
