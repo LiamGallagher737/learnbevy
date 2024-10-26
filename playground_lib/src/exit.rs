0b3f4b5e16a71b0827fd6008e73f64f376f0f363
Liam Gallagher <liam@liamgallagher.dev>
2024-10-26 21:45:35 +1300

    Revert "test stuff"
    This reverts commit 1f7431595aee56bb6e74371e8535b6a0d14d5eb8.

--- playground_lib/src/exit.rs
+++ playground_lib/src/exit.rs
@@ -7,15 +7,8 @@ static EXIT_FLAG: AtomicBool = AtomicBool::new(false);
 
 #[wasm_bindgen]
 pub fn exit() {
-    bevy_log::info!("exitttt");
     EXIT_FLAG.store(true, Ordering::Relaxed);
 }
-
-#[wasm_bindgen]
-pub fn bye() {
-    bevy_log::info!("byeeeee");
-}
-
 
 pub fn check_exit_flag(mut exit: EventWriter<AppExit>) {
     if EXIT_FLAG.load(Ordering::Relaxed) {
