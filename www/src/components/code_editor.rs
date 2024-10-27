c61a911efb7e8ae3b6124b3e812b8d0eed7bb3e3
Liam Gallagher <liam@liamgallagher.dev>
2024-10-27 17:50:18 +1300

    hide minimap

--- www/src/components/code_editor.rs
+++ www/src/components/code_editor.rs
@@ -1,7 +1,7 @@
 use crate::monaco::{setup_rust_plus_language, setup_vs_dark_plus_theme, RUST_PLUS, VS_DARK_PLUS};
 use dioxus::prelude::*;
 use dioxus_logger::tracing::info;
-use monaco::api::{CodeEditor, CodeEditorOptions};
+use monaco::{api::{CodeEditor, CodeEditorOptions}, sys::editor::IEditorMinimapOptions};
 use wasm_bindgen::JsCast;
 use web_sys::HtmlElement;
 
@@ -27,11 +27,16 @@ pub fn CodeEditor() -> Element {
         setup_vs_dark_plus_theme();
         setup_rust_plus_language();
 
-        let options = CodeEditorOptions::default()
+        let  options = CodeEditorOptions::default()
             .with_language(RUST_PLUS.to_owned())
             .with_theme(VS_DARK_PLUS.to_owned())
             .with_value(value.read().to_string())
-            .with_automatic_layout(true);
+            .with_automatic_layout(true)
+            .to_sys_options();
+
+        let minimap = IEditorMinimapOptions::default();
+        minimap.set_enabled(Some(false));
+        options.set_minimap(Some(&minimap));
 
         let code_editor = CodeEditor::create(&element, Some(options));
         editor.set(Some(code_editor));
