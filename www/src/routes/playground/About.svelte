9c10b2fe7a24d59eeaa463518606d70fefd3542d
Liam Gallagher <liam@liamgallagher.dev>
2024-11-02 16:08:05 +1300

    remove old section

--- www/src/routes/playground/About.svelte
+++ www/src/routes/playground/About.svelte
@@ -29,18 +29,6 @@
             </section>
 
             <section class="space-y-2">
-                <h4 class="text-md text-base font-semibold">How does it work?</h4>
-                <p class="text-muted-foreground">
-                    Every time you press the play button, the code in the editor along with your
-                    selected Bevy version and Rust channel are sent to my server. Once there some
-                    extra code is added and then it is compiled to wasm inside of a podman
-                    container. After its compiled, wasm-bindgen works it's magic on the output which
-                    is then sent back to your browser along with the generated javascript from
-                    wasm-bindgen and the stdout of the compile process.
-                </p>
-            </section>
-
-            <section class="space-y-2">
                 <h4 class="text-md text-base font-semibold">How can I contribute?</h4>
                 <p class="text-muted-foreground">
                     All the code is available publicly on GitHub at
