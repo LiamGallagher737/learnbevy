f1384c9fa6110a255a62d5483a26a115f5210986
Liam Gallagher <liam@liamgallagher.dev>
2024-11-02 16:07:51 +1300

    update page content

--- www/src/routes/playground/About.svelte
+++ www/src/routes/playground/About.svelte
@@ -13,6 +13,23 @@
         <div class="space-y-4">
             <section class="space-y-2">
                 <h4 class="text-md text-base font-semibold">How does it work?</h4>
+                <p class="pb-2 text-muted-foreground">
+                    The backend is hosted on a VPS from <a
+                        href="https://snakecrafthosting.com"
+                        class="text-primary underline"
+                        target="_blank">Snakecraft Hosting</a
+                    > who very generously sponsored this project. The server compiles and runs tools
+                    like clippy and bevy_lint on the code in your editor in docker containers. In the
+                    case of compiling, the WASM is sent back to your browser and ran like any other Bevy
+                    web game.
+                </p>
+                <a href="https://snakecrafthosting.com" alt="Snakecraft Hosting" target="_blank">
+                    <img src="https://my.snakecrafthosting.com/assets/img/logo.png" />
+                </a>
+            </section>
+
+            <section class="space-y-2">
+                <h4 class="text-md text-base font-semibold">How does it work?</h4>
                 <p class="text-muted-foreground">
                     Every time you press the play button, the code in the editor along with your
                     selected Bevy version and Rust channel are sent to my server. Once there some
@@ -33,11 +50,8 @@
                         target="_blank"
                     >
                         LiamGallagher737/learnbevy</a
-                    >.
-                </p>
-                <p class="text-muted-foreground">
-                    Remember that contributing doesn't mean having to write any code, it can be as
-                    simple as suggesting a new crate to be added!
+                    >. Feel free to have a go at some of the open issues or work on a new feature to
+                    improve the playground!
                 </p>
             </section>
         </div>
