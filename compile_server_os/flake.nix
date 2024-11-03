7e66481e18d45685023a4efa56fec9c1379c142a
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-11-03 14:47:11 +1300

    Remove old servers (#128)

--- compile_server_os/flake.nix
+++ compile_server_os/flake.nix
@@ -25,32 +25,5 @@
           ./configuration.nix
         ];
       };
-      nixosConfigurations.slc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; subdomain = "slc"; };
-        modules = [
-          disko.nixosModules.disko
-          { disko.devices.disk.disk1.device = "/dev/vda"; }
-          ./configuration.nix
-        ];
-      };
-      nixosConfigurations.deu = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; subdomain = "deu"; };
-        modules = [
-          disko.nixosModules.disko
-          {
-            disko.devices.disk.disk1.device = "/dev/vda";
-            networking.interfaces.ens3 = {
-              useDHCP = false;
-              ipv4.addresses = [
-                { address = "5.83.147.226"; prefixLength = 24; }
-              ];
-            };
-            networking.defaultGateway = "5.83.147.1";
-          }
-          ./configuration.nix
-        ];
-      };
     };
 }
