e432af95a6b14ae71a6aaa4175e81d747c226cdf
Liam Gallagher <liam@liamgallagher.dev>
2025-06-14 09:59:30 +1200

    commit

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<a href="/playground">Go to playground</a>
---
+++ website/src/routes/playground/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import * as Resizable from "@/components/ui/resizable";
+    import { Card } from "@/components/ui/card";
+</script>
+
+<div class="h-screen p-4">
+    <Resizable.PaneGroup direction="horizontal">
+        <Resizable.Pane defaultSize={70} minSize={40} class="flex flex-col gap-4">
+            One
+        </Resizable.Pane>
+
+        <Resizable.Handle withHandle class="mx-4" />
+
+        <Resizable.Pane defaultSize={30} minSize={20} class="flex flex-col gap-4">
+            <Card class="aspect-video"></Card>
+            <Card class="flex-grow overflow-auto p-4 font-mono text-sm"></Card>
+        </Resizable.Pane>
+    </Resizable.PaneGroup>
+</div>
